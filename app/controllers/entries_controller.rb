d8ee9644c446ebc72581cbe406e84ba20aea3570
ruchiramani <ruchiramani@gmail.com>
2016-04-19 09:45:16 -0400

    added random generator

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,19 +1,28 @@
 class EntriesController < ApplicationController
 
   def index
-
+     @all = Entry.all
+     random = rand(0..@all.length)
+     binding.pry
+     random_entry = @all[random]
   end
 
   def new
-
+   @entry = Entry.new
   end
 
   def create
-
+    @entry = Entry.create(entry_params)
+    redirect_to "/entries/index"
   end
 
   def show
 
   end
 
+  private
+
+  def entry_params
+   params.require(:entry).permit(:name, :memory, :quote, :grateful, :learned)
+  end
 end
